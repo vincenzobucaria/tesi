9617049d9915d4cc69de8ce9ec58098b4efb9735
vincenzobucaria <87119887+vincenzobucaria@users.noreply.github.com>
2023-08-02 12:03:22 +0200

    Add files via upload

--- network-generator.py
+++ network-generator.py
@@ -5,6 +5,7 @@ node_list = list()
 nat_dict = dict()
 private_network_topology = dict()
 public_network_topology = dict()
+import math
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
@@ -20,6 +21,8 @@ from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel 
 from mininet.node import OVSController
 
+public_network_subnets = dict()
+
 
 
 
@@ -29,6 +32,7 @@ def fileRead(fileName):
     network_file = open(fileName, "r")
     for line in network_file:
         line = line.strip('\n').split(' ')
+        print(line)
 
 
 
@@ -119,14 +123,33 @@ class network(Topo):
                 x = x+1
         def build_public_lan(lan, x):
             default_x = x
+            print("Nodi nella sottorete: ", len(public_network_topology[lan]))
+            subnet, end_ip = subnet_calculator(default_x, len(public_network_topology[lan]))
+            public_network_subnets[lan] = list()
+            public_network_subnets[lan].append(subnet)
+            print(public_network_subnets)
             print(default_x)
             for nodes in public_network_topology[lan]:
                 x = x+1
                 print(x)
                 print(nodes)
+                print("per ", nodes, "default-router: ", "9.0.0.{}".format(default_x))
                 host = self.addHost(nodes, ip="9.0.0.{}".format(x), defaultRoute='via 9.0.0.{}'.format(default_x))
                 self.addLink(host, self.switchdict[lan])
-            return x+1
+            return end_ip
+        
+        def subnet_calculator(default_ip, nodes):
+            log = math.log2(nodes+2)
+            result = int(math.ceil(log))
+            print("numero di ip necessari", nodes+2)
+            print("risultato", result)
+            if result == 1:
+                result = 2
+            end_ip = int(default_ip + math.pow(2, result))
+            return str("9.0.0.{}".format(default_ip)) + "/{}".format(32-result), end_ip
+
+
+
                 
         
         x = 0
@@ -144,7 +167,7 @@ class network(Topo):
 
         
 
-        y = 1
+        y = 0
         
     
         for lan in public_network_topology:
@@ -155,8 +178,37 @@ class network(Topo):
             self.addLink(self.routersdict[lan], self.switchdict[lan], intfName1='r{}-eth0'.format(x),params1={'ip': "9.0.0.{}/24".format(y)} )
             self.addLink(self.routersdict[lan], self.switchgenerale, cls=TCLink, delay='10ms', jitter='2ms', intfName1='r{}-eth1'.format(x), params1={'ip': "5.0.0.{}/24".format(x)}, use_htb=True)
             y = build_public_lan(lan, y)
+            public_network_subnets[lan].append("5.0.0.{}".format(x))
             x = x+1
+
+
+        #REGOLE DI ROUTING
+        
             
+    
+
+def network_setup(topo, net):
+    print("Funzione network_setup")
+    print(public_network_subnets)
+    for lan in private_network_topology:
+        #topo.routersdict[lan]
+        for route in public_network_subnets:
+            cmd= "sudo ip route add " + public_network_subnets[route][0] + " via " + public_network_subnets[route][1]
+            print("Per il router", topo.routersdict[lan], cmd)
+            print(net.get(topo.routersdict[lan]).cmd(cmd))
+
+    for lan in public_network_topology:
+        for route in public_network_subnets:
+            if route != lan:
+                cmd= "sudo ip route add " + public_network_subnets[route][0] + " via " + public_network_subnets[route][1]
+                print("Per il router", topo.routersdict[lan], cmd)
+                print(net.get(topo.routersdict[lan]).cmd(cmd))
+
+
+
+
+
+
 
 
 
@@ -169,6 +221,7 @@ test = network()
 
 net = Mininet(topo = test, controller = OVSController)
 net.start()
+network_setup(test, net)
 
 dumpNodeConnections(net.hosts)
 CLI(net)
